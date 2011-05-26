8fe1fc80d0327168ebef98df3c06d230954ebd8c
Shannon Rush <shannonmrush@gmail.com>
2011-05-25 19:00:37 -0600

    skeleton xcode project with core data

---
+++ foofaraw/foofarawAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  foofarawAppDelegate.h
+//  foofaraw
+//
+//  Created by Shannon Rush on 5/25/11.
+//  Copyright 2011 Rush Devo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface foofarawAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ foofaraw/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  foofaraw
+//
+//  Created by Shannon Rush on 5/25/11.
+//  Copyright 2011 Rush Devo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
